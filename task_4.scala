2ef22c2c9b0fe2ad25840b540153db013f67c1df
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2018-03-12 13:52:27 +0100

    Added logic to task 4

--- task_4.scala
+++ task_4.scala
@@ -1,11 +1,11 @@
 val tweetdb = sc.textFile("geotweets.tsv")
 val tweets = tweetdb.map(_.split('\t'))
-//UTC time in seconds
+//local UTC time in seconds:
+//val localTimesUTC = tweets.map(x => (x(1), (x(0).toFloat/1000 + x(8).toFloat)%24))
 val localTimesUTC = tweets.map(x => (x(1), x(0).toFloat/1000 + x(8).toFloat))
-localTimesUTC.take(3)
+localTimesUTC.take(6)
 println('\n')
-
-//val localTimezones = tweets.map(t => (t(0).toFloat/100 + t(8).toFloat)
+tweets.first()
 
 
 /*
@@ -15,3 +15,5 @@ println('\n')
 4. Sort time intervals from most to fewest tweets
 5. Write to output file 
 */
+
+//mapValue
