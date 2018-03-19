2009305d3b1dbb75aacc7cc8b66420bba3724bed
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2018-03-19 17:10:17 +0100

    Logic not finished. TODO: implement logic to group by country name

--- task_4.scala
+++ task_4.scala
@@ -1,21 +1,30 @@
 import java.util.Date
+import java.io._
+/*
+Sorterer timesintervallene etter antall tweets (høy til lav) globalt.
+
+TODO: Gjøre dette landspesifikt og printe topintervallet.
+Nåværende output til fil: "timeintervall 	antall"
+Ønsket output til fil: "land 	timeintervall 	antall"
+*/
 
 val tweetdb = sc.textFile("geotweets.tsv")
 val tweets = tweetdb.map(_.split('\t'))
-//local UTC time in seconds:
-//val localTimesUTC = tweets.map(x => (x(1), (x(0).toFloat/1000 + x(8).toFloat)%24))
-val localTimesUTC = tweets.map(x => (x(1), x(0).toFloat/1000 + x(8).toFloat))
-localTimesUTC.take(6)
-println('\n')
-tweets.first()
+//local UTC time divided in hours after midnight
+val localTimesUTC = tweets.map(x => (x(1), ((x(0).toFloat/1000 + x(8).toFloat)/3600) % 24)).
+					map(x => scala.math.floor(x._2).toInt).
+					countByValue().
+					toSeq.sortBy(x => -x._2).
+					take(1)
 
+val pw = new PrintWriter(new File("result_4.tsv"))
+localTimesUTC.foreach(x => pw.write(x._1 + "\t" + x._2 + "\n"))
+pw.close()
 
 /*
 1. Find local tweet times
 2. Find hourly time intervals (modulo 24)
-3. Categorize tweets in their time intevals
+3. Categorize tweets in their time intevals --> groupBy()
 4. Sort time intervals from most to fewest tweets
 5. Write to output file 
 */
-
-//mapValue
