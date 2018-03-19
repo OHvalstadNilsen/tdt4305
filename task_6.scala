d0e0fa8a0a559d1a735b9c221d361ce3805dab50
OHvalstadNilsen <oskar.h.nilsen@gmail.com>
2018-03-19 10:34:12 +0100

    Logic almost finished. Code needs review, as it is not completely correct.

--- task_6.scala
+++ task_6.scala
@@ -2,15 +2,19 @@ import scala.io.Source
 
 val rdd = sc.textFile("geotweets.tsv")
 val sample = rdd.sample(false, 0.1, 5)
-//Open stop words-file:
-//val stop_words = Source.fromFile("stop_words.txt").getLines.toList
+val stop_words = sc.textFile("stop_words.txt")
 
-val mostFrequentWords = sample.map(_.split('\t')).
-						map(x => (x(2), x(10)).
-						filter(x => x._2 == "US")
+val UStweets = sample.map(_.split('\t')).
+	map(x=> (x(2), x(10))).
+	filter(x => x._1 =="US").
+	map(x => x._2.toLowerCase)
 
+//Remove words:
+//FIXME: Remove words does not work properly
+//FIXME: Very slow. Can this be done more effectively?
+val stop_words_removed = UStweets.subtract(stop_words).
+	flatMap(x => x.split(" ")).
+	filter(w => w.length() > 1)
 
-/*
-	Filter(countrycode) --> split tweets into words --> filter out short words
-	and stop words --> sort by occurence frequency --> Return top ten
-*/
+val word_freqiencies = stop_words_removed.countByValue().
+	toSeq.sortBy(x => -x._2)
